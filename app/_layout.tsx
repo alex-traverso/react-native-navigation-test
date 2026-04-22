fdff3207ec295b21ebca8784c8e49a983f3a397a
alex traverso <alextraverso6@gmail.com>
2026-04-22 14:52:04 -0300

    add nativewind and add some test custom styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,32 @@
 import "@/global.css";
-import { Slot } from "expo-router";
-import React from "react";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen } from "expo-router";
+import React, { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Bold": require("../assets/fonts/WorkSans-Bold.ttf"),
+    "WorkSans-SemiBold": require("../assets/fonts/WorkSans-SemiBold.ttf"),
+    "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
+    "WorkSans-Regular": require("../assets/fonts/WorkSans-Regular.ttf"),
+    "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw new Error(`Failed to load fonts: ${error.message}`);
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return <Slot />;
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,26 @@
 import "@/global.css";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-200">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
-      </Text>
-
-      <Text style={{ color: "red", fontSize: 18, marginTop: 10 }}>
-        Prueba con style
-      </Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View className="flex-1 items-center justify-center bg-slate-200">
+        {/* <Text style={{ fontFamily: "WorkSans-Regular" }} className="text-xl ">
+          Welcome to Nativewind!
+        </Text> */}
+        <Text className="text-xl font-work-regular text-primary">
+          Welcome to Nativewind!
+        </Text>
+        <Text className="text-xl text-secondary">Welcome to Nativewind!</Text>
+        <Text className="text-xl font-work-regular text-secondary-100">
+          Welcome to Nativewind!
+        </Text>
+        <Text className="text-xl font-work-regular text-secondary-200">
+          Welcome to Nativewind!
+        </Text>
+        <Text className="text-xl text-tertiary ">Welcome to Nativewind!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
